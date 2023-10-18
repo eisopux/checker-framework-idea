cfc3d3aa1367bbcbfc55adad3b9057cc24c14cf5
Zain Salman <81134496+zainjamin@users.noreply.github.com>
2023-10-18 12:23:30 -0400

    basic implementation of checking for server and checker existence

--- src/main/kotlin/com/checkerframeworkidea/EcfLspServerDescriptor.kt
+++ src/main/kotlin/com/checkerframeworkidea/EcfLspServerDescriptor.kt
@@ -8,35 +8,51 @@ import java.io.File
 class EcfLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "ECF") {
     override fun createCommandLine(): GeneralCommandLine {
 
-        var serverInstalled = false
-        var checkerInstalled = false
+        val __dirname = System.getProperty("user.dir")
+        val serverFile = File(
+            File(__dirname).parent,
+            "download/checker-framework-languageserver-0.2.0-all.jar"
+        )
+        val checkerFile = File(
+            File(__dirname).parent,
+            "download/checker-framework-3.34.0-eisop1/checker/dist/checker.jar"
+        )
 
-        if (TODO("the server exists")) {
-            serverInstalled = true
-        }
-        if (TODO("the checker exists")) {
-            checkerInstalled = true
-        }
+        val serverInstalled = serverFile.exists()
+        val checkerInstalled = checkerFile.exists()
 
         if (!checkerInstalled || !serverInstalled) {
             downloadDeps()
+        } else {
+            println("good to go")
         }
 
         val commandLine = GeneralCommandLine()
         commandLine.withExePath("java")
 
         // Add classpath and server main class
-        val classpath = "/Users/ocen/ura-zain/checker-framework-idea/checker-framework-languageserver-0.2.0-all.jar:/Users/ocen/ura-zain/checker-framework-idea/checker-framework-3.34.0-eisop1/checker/dist/checker.jar"
+        val classpath = "${serverFile.absolutePath}:${checkerFile.absolutePath}"
         commandLine.addParameters("-cp", classpath)
         commandLine.addParameters("org.checkerframework.languageserver.ServerMain")
+
+        val checkerFrameworkPath = File(
+            File(__dirname).parent,
+            "download/checker-framework-3.34.0-eisop1"
+        ).absolutePath
+
+        println(
+            "classpath: $classpath\n"+
+            "cf path: $checkerFrameworkPath"
+        )
 
         // Add frameworkPath, checkers, and command line options
         commandLine.addParameters(
             "--frameworkPath",
-            "/Users/ocen/ura-zain/checker-framework-idea/checker-framework-3.34.0-eisop1",
+            checkerFrameworkPath,
             "--checkers",
             "org.checkerframework.checker.nullness.NullnessChecker",
         )
+        println(commandLine.getCommandLineString())
         return commandLine
     }
 
@@ -66,8 +82,16 @@ class EcfLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(
             destPath
         )
 
+        println("the downloader " + if (File(jarPath).exists()) "exists" else "does not exist")
+
         println("spawning downloader, args: $args")
-        val processBuilder = ProcessBuilder(args)
+        val processBuilder = ProcessBuilder(args).start()
+
+        processBuilder.inputReader().lines().forEach {
+            println(it)
+        }
+        val exitCode = processBuilder.waitFor()
+        println("exit code $exitCode")
         TODO("make download automatically")
     }
 }
